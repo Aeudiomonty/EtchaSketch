6d42dbd87ae6a2a4f280ecf8053d0dbe1b8d5eaf
Aeudiomonty <jacobjozefowicz@outlook.com>
2015-06-25 13:54:15 +0100

    'Start' button now creates grid.

--- js/javascript.js
+++ js/javascript.js
@@ -5,12 +5,30 @@ var axisY = 0;
 
 	$("#start").click(function(){
 
+		$(this).toggle(100);
+
 		axisX = prompt("Number of squares on the X axis.");
 		axisY = prompt("Number of squares on the Y axis.");
 
-		//Create grid.
-			//Create a main div.
-			//Create the small square divs.
+
+		
+			$("body").append("<div id='mainDiv'></div>");
+			$("#mainDiv").css({
+				"background-color": "black", 
+				"width": (axisX * 52) + "px", 
+				"height": (axisY * 52) + "px"
+			});
+
+
+
+			for(i = 0; i <= (axisX * axisY); i++)
+			{
+				$("#mainDiv").append("<div class='gridSquares'></div>");
+
+			}
+
+
+
 	});
 
 
