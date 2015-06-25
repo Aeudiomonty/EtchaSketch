86e52467f9762478102525d7269843a3f403bbc5
Aeudiomonty <jacobjozefowicz@outlook.com>
2015-06-25 13:12:01 +0100

    Created basic plan

--- js/javascript.js
+++ js/javascript.js
@@ -2,8 +2,19 @@ $(document).ready(function(){
 
 
 	$("button").onclick(function(){
-		//Create grid
+
+		//Use prompt to get number of squares on both axis.
+
+		//Create grid.
+			//Create a main div.
+			//Create the small square divs.
 	});
+
+
+	//Change colour of the squares in the grid when hovered over.
+
+
+	//Reset button.
 
 
 });
